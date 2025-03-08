3c751a3c58063f7eb7f2329a39511ca77d3d1736
Jeff Lee <somejeff@gmail.com>
2025-03-08 11:56:16 -0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders greeting", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const message = screen.getByText("Hello, John...");
+  expect(message).toBeInTheDocument();
 });
